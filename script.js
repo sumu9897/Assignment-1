0b2d6537c203987490932a7bb1e7c65b82272497
MOHAMMAD SUMON <mohammad.sumon9897@gmail.com>
2024-01-08 18:39:58 +0600

    Assignment 1

--- script.js
+++ script.js
@@ -1,36 +1,23 @@
-// Get the current year
-var currentYear = new Date().getFullYear();
-
-// Set the current year in the footer
-document.getElementById('currentYear').innerText = currentYear;
-
-// Your existing JavaScript code
-
-// script.js
-
 function checkAge() {
     var ageInput = document.getElementById("ageInput").value;
     var errorMessage = document.getElementById("error-message");
 
-    // Check if age is not provided
+
     if (ageInput === "") {
         errorMessage.textContent = "Please enter your age.";
         return;
     }
 
-    // Convert ageInput to a number
     var age = parseInt(ageInput);
 
-    // Check if the entered age is valid
     if (isNaN(age) || age < 0) {
         errorMessage.textContent = "Please enter a valid age.";
         return;
     }
 
-    // Clear any previous error messages
     errorMessage.textContent = "";
 
-    // Perform the age check and display the appropriate section
+
     if (age < 18) {
         showChildSection();
     } else {
@@ -48,9 +35,11 @@ function showAdultSection() {
     document.getElementById("adultSection").style.display = "block";
 }
 
-
-// Add event listener to the age input for the "input" event
 document.getElementById("ageInput").addEventListener("input", function() {
-    // Call the checkAge function whenever there is an input change
+
     checkAge();
 });
+
+var currentYear = new Date().getFullYear();
+
+document.getElementById('currentYear').innerText = currentYear;
